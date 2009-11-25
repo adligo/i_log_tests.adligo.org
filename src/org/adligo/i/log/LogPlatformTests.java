94408a706d4ba9d2418ddb8854c04f7da572ce75
scott <scott@adligo.com>
2009-11-25 12:26:56 +0000

    uptades to console printing when things go ary

--- src/org/adligo/i/log/LogPlatformTests.java
+++ src/org/adligo/i/log/LogPlatformTests.java
@@ -1,6 +1,9 @@
 package org.adligo.i.log;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
@@ -13,6 +16,7 @@ import org.mortbay.log.Log;
 
 public class LogPlatformTests extends TestCase implements I_SystemOutput {
 	private Throwable lastExceptionPrint = null;
+	private List<String> prints = new ArrayList<String>();
 	private boolean log = true;
 	
 	public void testInit() throws Exception {
@@ -45,27 +49,28 @@ public class LogPlatformTests extends TestCase implements I_SystemOutput {
 		
 		MockLogPlatform.unInit();
 		lastExceptionPrint  = null;
+		String dotFile = new File(".").getAbsolutePath();
+		String fileName = dotFile.substring(0, dotFile.length() - 2) + File.separator +
+			"no_file.properties";
+		
+		LogPlatform.init("/no_file.properties");
+		assertNull(lastExceptionPrint);
+		assertEquals(2, prints.size());
+		assertEquals("error reading file '/no_file.properties' " +
+				"system file 'null' using defaults;", prints.get(0));
+		assertEquals("using defaults {gwt_loggers=simple, defaultlog=INFO}", prints.get(1));
+		
 		URL url = LogPlatform.class.getResource("/adligo_log_log4j_factory.properties");
-		String fileName = url.getFile();
-		
-		
-		
-		LogPlatform.init("adligo_log_log4j_factory.properties");
-		String expected = "Error reading property file '/adligo_log_log4j_factory.properties'  file system name '" + fileName +"' file content; " +
-		"\nMapWrapper{defaultlog=DEBUG, log_factory=org.adligo.i.log.log4j.Log4jFactory}";
+		fileName = url.getFile();
+		MockLogPlatform.unInit();
+		prints.clear();
+		lastExceptionPrint = null;
+		LogPlatform.init("/adligo_log_log4j_factory.properties");
 		assertNotNull(lastExceptionPrint);
-		if (log) {
-			System.out.println("exception is; \n'" + lastExceptionPrint .getMessage() + "'");
-			System.out.println("expected exception is; \n'" +  expected + "'");
-		}
-		assertEquals(expected, 
-				lastExceptionPrint .getMessage());
-		
-		assertNotNull(lastExceptionPrint.getCause());
 		assertEquals("log_factory is null, because your code needs to call " +
 				"LogPlatform.addLogFactoryClass(String name, I_LogFactory p) " +
 				"with a valid instance of your logFactory org.adligo.i.log.log4j.Log4jFactory!", 
-				lastExceptionPrint.getCause().getMessage());
+				lastExceptionPrint.getMessage());
 	}
 
 	@Override
@@ -86,7 +91,7 @@ public class LogPlatformTests extends TestCase implements I_SystemOutput {
 
 	@Override
 	public void err(String p) {
-		assertTrue("Shouldn't have printing here " + p, false);
+		prints.add(p);
 	}
 
 	@Override
